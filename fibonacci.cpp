b20d8d55c0867cd0354621b6c5b9821e9742cad6
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-15 20:42:45 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
 using namespace std;
 
-//using Recurssion
-int fib(int n){
-    if(n<2){
-        return 1;
-    }
-    return fib(n-2) + fib(n-1);
+// using Recurssion
+int fib(int n)
+{
+	if (n < 2)
+	{
+		return 1;
+	}
+	return fib(n - 2) + fib(n - 1);
 }
 
-//using iterative method
-int fib(int n){
-    long long int a, b, c;
-    cout<<"(1) 1"<<endl;
-    if(n < 1){
-        cout<<"(2) 1"<<endl;
-    }
-    for(int i = 3; i <= n; i++){
-        c = a+b;
-        a = b;
-        b = c;
-        cout<<"("<<i<<")"<<c<<endl;
-    }
+// using iterative method
+int fib(int n)
+{
+	long long int a, b, c;
+	cout << "(1) 1" << endl;
+	if (n < 1)
+	{
+		cout << "(2) 1" << endl;
+	}
+	for (int i = 3; i <= n; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		cout << "(" << i << ")" << c << endl;
+	}
 }
 
-int main(){
-    int a;
-    // cout<<"Enter the number : ";
-    // cin>>a;
-    // cout<<"The number on the position "<<a<<" in fibonacci series is : "<<fib(a);
+int main()
+{
+	int a;
+	// cout<<"Enter the number : ";
+	// cin>>a;
+	// cout<<"The number on the position "<<a<<" in fibonacci series is : "<<fib(a);
 
-    cout<<"Enter the number of terms you want to display in fibonacci series : ";
-    cin>>a;
-    cout<<"The fibonacci series is as follows : ";
-    for(int i = 0; i < a; i++){
-        cout<<fib(i)<<" ";
-    } 
+	cout << "Enter the number of terms you want to display in fibonacci series : ";
+	cin >> a;
+	cout << "The fibonacci series is as follows : ";
+	for (int i = 0; i < a; i++)
+	{
+		cout << fib(i) << " ";
+	}
 
-    return 0;
+	return 0;
 }
